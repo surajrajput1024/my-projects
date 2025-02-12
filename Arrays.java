ef56a71b8ea15a008965a92d885d53fdddd5b50f
Prof <professor@uixlabs.in>
2025-02-12 13:46:53 +0530

    Next permutation leetcode 31

--- Arrays.java
+++ Arrays.java
@@ -6,29 +6,36 @@ public class Arrays {
      * 
      * Time complexity: O(3N)
      * Space Complexity: O(1)
+     * Example: 4 1 5 3 2
      * @param nums
      */
     private static void nextPermutation(int[] nums) {
-        int firstIndex = 0; // Index to track where 
-        int secondIndex = 0;
+        int firstIndex = -1; // Index to track where we've found longest similar prefix 1 2 3, 1 3 2 so 1 is longest one
+        int secondIndex = -1; // from which element to swap with the first index element
         int n = nums.length;
 
-        for(int i = n-1; i>0; i--) {
-          if(nums[i] > nums[i-1]) {
+        // find first index by observing pattern from right --> left, first index = 1 that is element 1
+        for(int i = n-2; i>=0; i--) {
+          if(nums[i] < nums[i+1]) {
             firstIndex = i;
             break;
           }
         }
 
-        for(int i = n-1; i >0; i--) {
-            if(nums[i] > nums[firstIndex]) {
+        // find second index, second index is 4 that is element 2. 
+        for(int i = n - 1; i>=0; i--) {
+            if(firstIndex != -1 && nums[i] > nums[firstIndex]) {
                 secondIndex = i;
                 break;
             }
         }
 
-        swap(nums, firstIndex, secondIndex);
+        // swap fist and second index elements, second index is, now array: 4 2 5 3 1
+        if(firstIndex != secondIndex && firstIndex != -1 || secondIndex != -1) {
+            swap(nums, firstIndex, secondIndex);
+        }
 
+        // reverse from first index + 1 to end of the array: array now - 4 2 1 3 5 
         reverse(nums,firstIndex + 1, n - 1);
     }
 
