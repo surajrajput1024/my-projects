db55b407478ae055277855692887c5cc83d9a83f
Suraj Singh Rajput AKA Prof <00suraj.sig@gmail.com>
2025-03-03 01:59:16 +0530

    1710. Maximum Units on a Truck

---
+++ dsaproblems/1710:MaximumUnitsOnATruck.java
@@ -0,0 +1,29 @@
+package dsaproblems;
+
+import java.util.List;
+
+import algorithms.ArrayList;
+
+
+public class 1710:MaximumUnitsOnATruck {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
+
+        int maxUnits = 0;
+        for (int[] box : boxTypes) {
+            int boxesToTake = Math.min(truckSize, box[0]);
+            maxUnits += boxesToTake * box[1];
+            truckSize -= boxesToTake;
+            if (truckSize == 0) break; 
+        }
+
+        return maxUnits;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] boxTypes = {{1,3}, {2,2}, {3,1}};
+        int truckSize = 4;
+        System.out.println(sol.maximumUnits(boxTypes, truckSize)); 
+    }
+}
