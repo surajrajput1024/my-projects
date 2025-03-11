67ea963fd003d4d5c6b4c9bdc2bf0e096a29b3e8
Suraj Singh Rajput AKA Prof. <00suraj.sig@gmail.com>
2025-03-12 03:42:37 +0530

    strategy design pattern

---
+++ designpatterns/StrategyDesignPatterns.java
@@ -0,0 +1,24 @@
+package designpatterns;
+
+/**
+ * Strategy Design Pattern: Strategy Design Pattern is a behavioral design pattern that allows us to define a family of
+ * algorithms, encapsulate each algorithm, and make them interchangeable. The Strategy pattern lets the algorithm vary
+ * independently from the clients that use it.
+ * 
+ * Real world example:
+ * 1. Sorting algorithms: Sorting algorithms are a real world example of Strategy Design Pattern. In sorting algorithms,
+ *    there are many algorithms like bubble sort, selection sort, insertion sort, quick sort, merge sort, etc. Each sorting
+ *    algorithm is encapsulated in a separate class and the client can choose the algorithm that they want to use.
+ * 
+ * 2. Payment methods: Payment methods are another real world example of Strategy Design Pattern. In payment methods, there
+ *   are many payment methods like credit card, debit card, net banking, UPI, etc. Each payment method is encapsulated in
+ *  a separate class and the client can choose the payment method that they want to use.
+ * 
+ * 3. Travel booking: Travel booking is another real world example of Strategy Design Pattern. In travel booking, there are
+ *   many travel booking websites like MakeMyTrip, Goibibo, Yatra, etc. Each travel booking website has different
+ * algorithms for booking flights, hotels, buses, etc. The client can choose the travel booking website that they want to
+ * use.
+ */
+public class StrategyDesignPatterns {
+    
+}
